18cdbe80e1b8495ac58b5dbc7878f937ca11ced1
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-05 11:53:45 -0300

    EX02 - feat: Criação da classe de pessoas e do metodo de inserir nomes.

---
+++ EX02/Entrada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Miscelania;
+
+namespace Entrada
+{
+    public static class Pessoas
+    {
+        public static string InserirNome(int numPessoa)
+        {
+            while (true)
+            {
+                Console.Write($"Digite o nome da pessoa {numPessoa}: ");
+                string? inputOriginal = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(inputOriginal)) continue;
+
+                inputOriginal = inputOriginal.Trim();
+
+                if (Regex.IsMatch(inputOriginal, @"^[\p{L}\s]{2,}$")) return inputOriginal;
+
+                Limpar.LimparTela("Entrada inválida, verifique se o que foi inserido não haja números e nenhum caracter especial.");
+            }
+        }
+    }
+}
