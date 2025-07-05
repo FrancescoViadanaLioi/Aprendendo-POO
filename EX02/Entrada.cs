50273a6377f97f1a74d3a6a71fb78a22aa92ddff
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-05 12:06:47 -0300

    EX02 - feat: alteração da classe de Pessoas para PessoaInput, e a criação do método responsável pela leitura de idade. Além disso, a classe Pessoa agora tem variáveis públicas, as quais são nome e idade.

--- EX02/Entrada.cs
+++ EX02/Entrada.cs
@@ -4,22 +4,40 @@ using Miscelania;
 
 namespace Entrada
 {
-    public static class Pessoas
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+
+    public static class PessoaInput
     {
         public static string InserirNome(int numPessoa)
         {
             while (true)
             {
                 Console.Write($"Digite o nome da pessoa {numPessoa}: ");
-                string? inputOriginal = Console.ReadLine();
+                string? inputOriginal = Console.ReadLine()?.Trim();
 
                 if (string.IsNullOrWhiteSpace(inputOriginal)) continue;
-
-                inputOriginal = inputOriginal.Trim();
 
                 if (Regex.IsMatch(inputOriginal, @"^[\p{L}\s]{2,}$")) return inputOriginal;
 
                 Limpar.LimparTela("Entrada inválida, verifique se o que foi inserido não haja números e nenhum caracter especial.");
+            }
+        }
+        public static int InserirIdade(int numPessoa)
+        {
+            while (true)
+            {
+                Console.Write($"Digite a idade da pessoa {numPessoa}: ");
+                string? input = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(input)) continue;
+
+                if (int.TryParse(input, out int idade) && (idade >= 0 && idade <= 120)) return idade;
+
+                Limpar.LimparTela("Entrada inválida, verifique se você digitou um número inteiro maior ou igual a zero.");
             }
         }
     }
