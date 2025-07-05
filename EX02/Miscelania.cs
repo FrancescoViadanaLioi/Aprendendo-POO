40e47b834bfbc031e9c51e0d333ca88bbfa4626f
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-05 11:27:40 -0300

    EX02 - feat: Criação da classe de limpeza

---
+++ EX02/Miscelania.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class Limpar
+    {
+        public static void LimparTela(string msgErro)
+        {
+            Console.WriteLine(msgErro);
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
