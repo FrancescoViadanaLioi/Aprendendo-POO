7f7d918209eab9dd3433b49be91daa8ad470e21a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-07 17:01:37 -0300

    EX03 - feat: Script de miscelânia que controla a UX.

---
+++ EX03/Miscelania.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class Limpeza
+    {
+        public static void LimparTela(string msgErro)
+        {
+            Console.WriteLine(msgErro);
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
