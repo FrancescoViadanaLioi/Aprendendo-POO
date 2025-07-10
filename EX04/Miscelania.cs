5c346df263ea74a8eac1d8a0db23dc2373742d3d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-10 18:10:07 -0300

    EX04 - feat: Script responsável pela UI do Programa.

---
+++ EX04/Miscelania.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErroLimpar(string msgErro)
+        {
+            Console.WriteLine(msgErro);
+            AguardarEntrada();
+            Console.Clear();
+        }
+        public static void PausarComMensagem(string msgPausa)
+        {
+            Console.WriteLine(msgPausa);
+            AguardarEntrada();
+        }
+        private static void AguardarEntrada()
+        {
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey(true);
+        }
+    }
+}
