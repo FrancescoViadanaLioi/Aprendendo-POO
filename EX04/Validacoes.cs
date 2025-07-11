99c464d10fa9d3f6d49be36503cd1b9ced96087a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-11 10:14:25 -0300

    EX04 - feat: Criação do script e da classe de validações.

---
+++ EX04/Validacoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Validacoes
+{
+    public class Validations
+    {
+        public static bool NomeEhValido(string? nome)
+        {
+            return !string.IsNullOrWhiteSpace(nome) && Regex.IsMatch(nome, @"^[A-Za-zÀ-ÿ\s]+$");
+        }
+    }
+}
