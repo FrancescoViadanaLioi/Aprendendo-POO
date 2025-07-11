d79dc02220297e33fac96ef69bc416ca67e70d3c
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-11 10:32:06 -0300

    EX04 - feat: Criação de script, classes e métodos que validam a entrada do usuário para nome e preço do produto.

---
+++ EX04/InserirDados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using Produto;
+using Validacoes;
+using Miscelania;
+
+namespace InserirDados
+{
+    public class RegistarProduto
+    {
+        public string? RegistrarNome()
+        {
+            while (true)
+            {
+                string? input = ExibirRequisicao("Digite o nome do produto: ");
+                bool ehValido = Validations.NomeEhValido(input);
+                if (ehValido) return input?.Trim();
+                ConsoleUI.ExibirErroLimpar("Erro! Verifique se não há espaços em branco desnecessários e nenhum número.");
+            }
+        }
+        public double RegistrarPreco()
+        {
+            while (true)
+            {
+                string? input = ExibirRequisicao("Digite o preço do produto: ");
+                bool ehValido = Validations.PrecoEhValido(input);
+                if (ehValido) return double.Parse(input!, CultureInfo.InvariantCulture);
+                ConsoleUI.ExibirErroLimpar("ERRO! Verifique se você digitou somente um número positivo, sem letras ou símbolos.");
+            }
+        }
+
+        private static string? ExibirRequisicao(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadLine();
+        }
+    }
+}
