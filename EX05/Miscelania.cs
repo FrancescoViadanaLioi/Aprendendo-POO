d0034a7ecac7ec8c43634c88ab884a12fff8a6d9
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-14 15:20:21 -0300

    EX05 - feat: Namespace de Miscelania e Mensagens de UI.

---
+++ EX05/Miscelania.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErroELimpar(string msgErro)
+        {
+            Console.WriteLine(msgErro);
+            AguardarEntrada();
+            Console.Clear();
+        }
+        public static void SairPausa(string msg)
+        {
+            Console.WriteLine(msg);
+            AguardarEntrada();
+        }
+        private static void AguardarEntrada()
+        {
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+        }
+    }
+}
