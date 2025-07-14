0f78f4dc39a4118e1e92ee6b27399339e3e9fb38
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-14 15:51:16 -0300

    EX05 - feat: Classe de entrada do usuário.

---
+++ EX05/TrianguloInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Triangulo;
+using Validacoes;
+using Miscelania;
+
+namespace TrianguloInput
+{
+    public class LadosTriangulo
+    {
+        public double LerLado(string lado)
+        {
+            while (true)
+            {
+                string? input = ExibirRequisicao($"Insira o lado {lado} do triângulo: ");
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    ConsoleUI.ExibirErroELimpar("ERRO! O valor não pode ser vazio.");
+                    continue;
+                }
+                bool sucesso = Validations.ValidarLado(input);
+                if (sucesso) return double.Parse(input);
+                ConsoleUI.ExibirErroELimpar("ERRO! Verifique se o valor é um número real positivo.");
+            }
+        }
+        private string? ExibirRequisicao(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadLine();
+        }
+    }
+}
