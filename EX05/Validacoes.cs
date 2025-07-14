f00cc0a8d23d8149657889486c3a34da0134200d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-14 15:38:09 -0300

    EX05 - feat: Classe de validação do lado do triângulo.

---
+++ EX05/Validacoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Validacoes
+{
+    public class Validations
+    {
+        public static bool ValidarLado(string input)
+        {
+            return double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out double lado);
+        }
+    }
+}
