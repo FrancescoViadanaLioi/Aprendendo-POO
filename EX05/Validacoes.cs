f1ae6af23e259d6997198b4b005ed45596b9e907
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-14 18:06:38 -0300

    EX05 - feat: Adição do validador de triângulo.

--- EX05/Validacoes.cs
+++ EX05/Validacoes.cs
@@ -9,5 +9,10 @@ namespace Validacoes
         {
             return double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out double lado);
         }
+        public static bool ValidarTriangulo(double a, double b, double c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a) return false;
+            return true;
+        }
     }
 }
