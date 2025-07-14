ba3381e22ac0da792040caea1235fb3029c17129
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-14 18:13:37 -0300

    EX05 - feat: Adição de métodos de cálculo de área e perímetro.

--- EX05/Triangulo.cs
+++ EX05/Triangulo.cs
@@ -7,5 +7,15 @@ namespace Triangulo
         public double A { get; set; }
         public double B { get; set; }
         public double C { get; set; }
+   
+    public static double CalcularArea(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2.0;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+        public static double CalcularPerimetro(double a, double b, double c)
+        {
+            return a + b + c;
+        }
     }
 }
