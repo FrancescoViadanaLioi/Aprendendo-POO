c8f45cf05ef445a64dd4b2370203975ede319a30
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 19:20:12 -0300

    EX07 - feat: Classe de recursos universais, com um método de pedir requisição.

---
+++ EX07/RecursosUniversais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RecursosUniversais
+{
+    public static class UniversalResources
+    {
+        public static string? ExibirRequisicao(string texto)
+        {
+            Console.Write(texto);
+            return Console.ReadLine();
+        }
+    }
+}
