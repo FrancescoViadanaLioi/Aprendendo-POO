00b1d31ae0f43cca73de1cac0a4f6e7a8d37af29
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 14:19:13 -0300

    EX06 - feat: Classe que é responsável pela requisição de entrada do usuário.

---
+++ EX06/RetanguloInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using RecursosUniversais;
+using Validacoes;
+using Miscelania;
+
+namespace RetanguloInput
+{
+    public static class EntradaRetangulo
+    {
+        public static double LerEntrada(string item)
+        {
+            while (true)
+            {
+                string? input = UniversalResources.ExibirRequisição($"Insira o valor da {item} do retângulo: ");
+                bool ehValido = Validations.MedidaEhValida(input);
+                if (ehValido) return double.Parse(input!, CultureInfo.InvariantCulture);
+                else ConsoleUI.ExibirErro($"ERRO! Verifique se a entrada é um número real positivo.");
+            }
+        }
+    }
+}
