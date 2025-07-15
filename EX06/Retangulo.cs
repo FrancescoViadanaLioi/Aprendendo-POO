178723e886e7c9436dc9983b203250d8dd9c27e7
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 12:42:40 -0300

    EX06 - feat: Classe do retângulo, contendo suas medidas e seus cálculos de Perímetro e diagonal.

---
+++ EX06/Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Retangulo
+{
+    public class Retangle
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public static double CalcularPerimetro(double Largura, double Altura)
+        {
+            return Largura + Altura;
+        }
+        public static double CalcularDiagonal(double Largura, double Altura)
+        {
+            double somaPot = SomarAsPotencias(Largura, Altura);
+            return Math.Sqrt(somaPot);
+        }
+        private static double SomarAsPotencias(double Largura, double Altura)
+        {
+            return Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
+        }
+    }
+}
