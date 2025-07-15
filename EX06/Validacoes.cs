0b4b99bc1bfd610137e5485ff0e00c0ddf209c09
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 14:18:29 -0300

    EX06 - feat: Classe responsável por validação de entrada.

---
+++ EX06/Validacoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Validacoes
+{
+    public static class Validations
+    {
+        public static bool MedidaEhValida(string? tamanho)
+        {
+            if (string.IsNullOrWhiteSpace(tamanho)) return false;
+            bool sucesso = double.TryParse(tamanho, CultureInfo.InvariantCulture, out double numero);
+            return sucesso && numero > 0;
+        }
+    }
+}
