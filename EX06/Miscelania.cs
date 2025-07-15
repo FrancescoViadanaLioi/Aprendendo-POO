442e856b316d79168d9f28ade3905909549ee82c
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 12:22:09 -0300

    EX06 - feat: Classe de ConsoleUI, responsável por exibir mensagem de erro e de pausa, se necessária.

---
+++ EX06/Miscelania.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErro(string msgErro)
+        {
+            Console.WriteLine(msgErro);
+            AnyKey();
+            Console.Clear();
+        }
+        public static void PausarEContinuar(string msg)
+        {
+            Console.WriteLine(msg);
+            AnyKey();
+        }
+        private static void AnyKey()
+        {
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+        }
+    }
+}
