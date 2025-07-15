a09e095a3188671a43c165d753a25a6f3517988a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 13:33:45 -0300

    EX06 - feat: Classe que contem recursos universais do programa, como um script para delegar requisição.

---
+++ EX06/RecursosUniversais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RecursosUniversais
+{
+    public static class UniversalResources
+    {
+        public static string? ExibirRequisição(string requisicao)
+        {
+            Console.Write(requisicao);
+            return Console.ReadLine();
+        }
+    }
+}
