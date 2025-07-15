62b340f3304d486c41e751de0d3753d77f331e28
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 18:34:12 -0300

    EX07 - feat: Classe de Mensagens do usuário e de formatação de casa decimal.

---
+++ EX07/Miscelania.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErro(string msgErro)
+        {
+            MensagemExibir(msgErro);
+            Console.Clear();
+        }
+        public static void MensagemExibir(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+        }
+    }
+    public static class Formatador
+    {
+        public static string? FormatarCasaDecimal(double valor)
+        {
+            return valor.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
