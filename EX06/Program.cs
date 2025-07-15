7b460a58bfd84a0d80c47a0eea144af460e615b3
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-15 15:29:24 -0300

    EX06 - feat: Integração do programa ao Main.

---
+++ EX06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Retangulo;
+using RetanguloInput;
+using Miscelania;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var retangulo = new Retangle();
+        retangulo.Largura = EntradaRetangulo.LerEntrada("Largura");
+        retangulo.Altura = EntradaRetangulo.LerEntrada("Altura");
+        double perimetroRetangulo = Retangle.CalcularPerimetro(retangulo.Largura, retangulo.Altura);
+        double diagonalRetangulo = Retangle.CalcularDiagonal(retangulo.Largura, retangulo.Altura);
+
+        Console.WriteLine($"O retângulo, de medidas {retangulo.Largura}m e {retangulo.Altura}, seu perímetro é {Formatador.FormatarDuasCasas(perimetroRetangulo)}m, e sua diagonal mede {Formatador.FormatarDuasCasas(diagonalRetangulo)}m.");
+        ConsoleUI.PausarEContinuar("Fim do programa.");
+    }
+}
