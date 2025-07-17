054ac4e5eb9102ea83b826f57d2d25075f6d38ef
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-17 13:17:26 -0300

    EX07 - fix: ajuste do cálculo de reajuste salarial.

---
+++ EX07/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Funcionario
+{
+    public class AtributosFuncionario
+    {
+        public string? Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido(double salarioBruto, double imposto)
+        {
+            return salarioBruto - imposto;
+        }
+        public double AjusteDeSalario(double salarioBruto, double porcentagem)
+        {
+            return salarioBruto + salarioBruto * (porcentagem / 100);
+        }
+    }
+}
