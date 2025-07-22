f4d5f2464e171ca519a20635231012959b05fe47
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 10:36:37 -0300

    EX08 - fix: Correção de código.

--- EX08/Validacoes.cs
+++ EX08/Validacoes.cs
@@ -4,7 +4,7 @@ namespace Validations
 {
     public static class Validacoes
     {
-        public static bool ValidarDecimal(string? input, double num)
+        public static bool ValidarDecimal(string? input, out double num)
         {
             return double.TryParse(input, out num) && num >= 1;
         }
