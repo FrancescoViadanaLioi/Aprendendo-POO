d016a909698303c358f421e72eea844ed3678a23
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 10:07:17 -0300

    EX08 - feat: Classe responsável pela informações básicas do dinheiro no contexto do exercício, e método de conversão de moeda.

---
+++ EX08/Dinheiro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Dinheiro
+{
+    public class Money
+    {
+        public double Aporte { get; set; }
+        public double CotacaoDolar { get; set; }
+
+        public double ConverterDolar(double cotDolar)
+        {
+            return Aporte * cotDolar;
+        }
+    }
+}
