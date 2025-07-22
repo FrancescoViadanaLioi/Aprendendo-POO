2db104bb48f9b60b93794e31f39c169a7c6f7cce
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 09:44:00 -0300

    EX08 - feat: Classe de Requisição, que exibe a mensagem de requisiçao da entrada do usuário.

---
+++ EX08/RecursosUniversais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RecursosUniversais
+{
+    public static class Requisicoes
+    {
+        public static string? ExibirRequisicao(string input)
+        {
+            Console.Write(input);
+            return Console.ReadLine();
+        }
+    }
+}
