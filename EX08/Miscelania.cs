d99f74015f7ef7efdc33b3aec7711005a6e33f32
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 09:33:59 -0300

    EX08 - feat: Classe de ConsoleUI, que limpa o console em caso de erro.

---
+++ EX08/Miscelania.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErroELimpar(string msgErro)
+        {
+            Console.WriteLine($"ERRO! {msgErro}");
+            PressAnyKey();
+            Console.Clear();
+        }
+        private static string? PressAnyKey()
+        {
+            Console.WriteLine("(Pressione qualquer tecla para contiunar)");
+            return Console.ReadLine();
+        }
+    }
+}
