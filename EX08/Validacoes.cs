29f2f1197d3a914cee1066598ce6d44567d61c31
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 10:15:51 -0300

    EX08 - feat: Classe de validação. O método valida o número decimal inserido.

---
+++ EX08/Validacoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Validations
+{
+    public static class Validacoes
+    {
+        public static bool ValidarDecinal(string? input, double num)
+        {
+            return double.TryParse(input, out num);
+        }
+    }
+}
