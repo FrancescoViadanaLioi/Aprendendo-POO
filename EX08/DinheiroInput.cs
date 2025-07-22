0e59633f370acd65301fdbd486c2baabd1926f25
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-22 11:41:38 -0300

    EX08 - feat: Classe responsável por exibir a requisição de entrada e a validação dela.

---
+++ EX08/DinheiroInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Validations;
+using RecursosUniversais;
+using Miscelania;
+
+namespace DinheiroInput
+{
+    public static class Entrada
+    {
+        public static double EntrarComPedido(string msg)
+        {
+            while (true)
+            {
+                string? input = Requisicoes.ExibirRequisicao(msg);
+                bool sucesso = Validacoes.ValidarDecimal(input, out double valor);
+                if (sucesso) return valor;
+                ConsoleUI.ExibirErroELimpar("Verifique se o valor inserido é um número válido (número positivo decimal).");
+            }
+        }
+    }
+}
