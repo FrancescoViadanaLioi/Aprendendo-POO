233f2bda21c32fdb6893de1945acd9d7740e3d3d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-30 11:55:07 -0300

    EX09 - feat: Classe de Recursos Universais, no momento, contendo o método de Exibir a requisição do usuário.

---
+++ EX09/RecursosUniversais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UniversalResources
+{
+    public static class RecursosUniversais
+    {
+        public static string? ExibirRequisicao(string texto)
+        {
+            Console.WriteLine(texto);
+            return Console.ReadLine();
+        }
+    }
+}
