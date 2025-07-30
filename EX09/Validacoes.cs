b5dc94575d8806e503f1ac8709e214b2ac35dcce
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-30 11:41:12 -0300

    EX09 - feat: Classe responsável por validar entradas do usuário.

---
+++ EX09/Validacoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Validations
+{
+    public static class Validacoes
+    {
+        public static bool ValidarNome(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, @"^[A-Za-zÀ-ÿ\s]+$");
+        }
+        public static bool ValidarPreco(string input, out double preco)
+        {
+            return double.TryParse(input, CultureInfo.InvariantCulture, out preco) && preco > 0;
+        }
+        public static bool ValidarQuantidade(string input, out int quantidade)
+        {
+            return int.TryParse(input, out quantidade);
+        }
+    }
+}
