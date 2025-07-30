a51ff6c82ac970756b67a9ba69dadb9646f8c94e
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-30 12:01:20 -0300

    EX09 - feat: Classe ConsoleUI feita para exibir mensagens que proporcionam uma melhor UX.

---
+++ EX09/Miscelania.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void ExibirErro(string msgErro)
+        {
+            ExibirMensagem(msgErro);
+            Console.Clear();
+        }
+        public static void ExibirMensagem(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("(Pressione qualquer tecla para continuar)");
+            Console.ReadKey();
+        }
+    }
+}
