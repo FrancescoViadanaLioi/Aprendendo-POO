3e47ff4686b654e025a3cf9e98c1a7a90de87071
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-07-30 11:33:56 -0300

    EX09 - feat: Classe responsável pelas informações e operações do produto.

---
+++ EX09/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProdutoInfo
+{
+    public class Produto
+    {
+        public string? Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string? nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public static int AlterarEstoque(int quantidade, int variacao)
+        {
+            return quantidade + variacao;
+        }
+    }
+}
