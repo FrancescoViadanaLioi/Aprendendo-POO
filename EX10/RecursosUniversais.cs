a72625300a0f4644219b5808ab6dc2780e43c1d9
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-13 09:08:20 -0300

    EX10 - feat: Classe de requisições de entrada, com um método que exibe uma mensagem e retorna com a leitura do input de usuário.

---
+++ EX10/RecursosUniversais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UniversalResources
+{
+    public static class Requisicao
+    {
+        public static string? PedirEntrada(string requisicao)
+        {
+            Console.Write(requisicao);
+            return Console.ReadLine()!;
+        }
+    }
+}
