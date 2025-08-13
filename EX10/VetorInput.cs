5a248586ca137528373044b2ffbf3db6762840be
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-13 13:05:08 -0300

    EX10 - feat: Métodos que pedem o tamanho do vetor e os elementos pertencentes a ele.

---
+++ EX10/VetorInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UniversalResources;
+using Miscelania;
+using Validacoes;
+
+namespace Vetor.Input
+{
+    public static class EntradaDoUsuario
+    {
+        public static int PedirTamanhoVetor()
+        {
+            while (true)
+            {
+                string? input = Requisicao.PedirEntrada("Qual é o tamanho do vetor? ");
+                if (ValidacoesDeDados.ValidarTamanhoVetor(input!, out int tamanho)) return tamanho;
+                ConsoleUI.ExibirErro("Verifique se o tamanho inserido é um valor positivo maior do que zero.");
+            }
+        }
+        public static int[] PedirValores(int n)
+        {
+            int[] vector = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                while (true)
+                {
+                    string? input = Requisicao.PedirEntrada($"Qual o valor do elemento {i}: ");
+                    if (ValidacoesDeDados.ValidarElemento(input!))
+                    {
+                        vector[i] = int.Parse(input!);
+                        break;
+                    }
+                    ConsoleUI.ExibirErro("Verifique se o elemento do vetor é um número inteiro.");
+                }
+            }
+            return vector;
+        }
+    }
+}
