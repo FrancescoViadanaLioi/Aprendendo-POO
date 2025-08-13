e3aa37393d6a8ff8f6b3a2408b8e1dd3e09be4ca
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-13 09:36:48 -0300

    EX10 - feat: Adição de métodos de fábrica para abstrair a impressão de mensagens ao usuário.

--- EX10/Mensagem.cs
+++ EX10/Mensagem.cs
@@ -13,16 +13,24 @@ namespace Miscelania
     public class Mensagem
     {
         public string Conteudo { get; }
-        public TipoMensagem Tipo { get ; }
+        public TipoMensagem Tipo { get; }
         public bool EsperarTecla { get; }
         public bool LimparConsole { get; }
 
         public Mensagem(string conteudo, TipoMensagem tipo, bool esperarTecla, bool limparConsole)
         {
-            Conteudo = conteudo;
+            Conteudo = conteudo ?? throw new ArgumentNullException(nameof(conteudo));
             Tipo = tipo;
             EsperarTecla = esperarTecla;
             LimparConsole = limparConsole;
         }
+        public static Mensagem Alerta(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Alerta, esperarTecla, limparConsole);
+        public static Mensagem Erro(string conteudo, bool esperarTecla = true, bool limparConsole = true)
+            => new Mensagem(conteudo, TipoMensagem.Erro, esperarTecla, limparConsole);
+        public static Mensagem Informacao(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Informacao, esperarTecla, limparConsole);
+        public static Mensagem Sucesso(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Sucesso, esperarTecla, limparConsole);
     }
 }
