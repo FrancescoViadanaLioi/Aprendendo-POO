fcf40a6881c5cd627c7484ed472e5ec97ac6dd87
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-13 09:06:32 -0300

    EX10 - feat: Script que exibe as mensagens ao usuário quando é chamado. Além disso, possui um método privado que colore as mensagens dependendo de seu tipo.

---
+++ EX10/Miscelania.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Miscelania;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void Exibir(Mensagem mensagem)
+        {
+            var CorOriginal = Console.ForegroundColor;
+            Console.ForegroundColor = Colorir(mensagem.Tipo);
+            string prefixo = $"{mensagem.Tipo.ToString().ToUpper()}!";
+            Console.WriteLine($"{prefixo} {mensagem.Conteudo}");
+
+            Console.ForegroundColor = CorOriginal;
+
+            if (mensagem.EsperarTecla)
+            {
+                Console.WriteLine("(Pressione qualquer tecla para continuar)");
+                Console.ReadKey();
+            }
+            if (mensagem.LimparConsole)
+            {
+                Console.Clear();
+            }
+        }
+        private static ConsoleColor Colorir(TipoMensagem tipo)
+        {
+            return tipo switch
+            {
+                TipoMensagem.Alerta => ConsoleColor.Yellow,
+                TipoMensagem.Erro => ConsoleColor.Red,
+                TipoMensagem.Informacao => ConsoleColor.Blue,
+                TipoMensagem.Sucesso => ConsoleColor.Green,
+                _ => ConsoleColor.White
+            };
+        }
+    }
+}
