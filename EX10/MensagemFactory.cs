d1e7894372e81dde67f92d023e84240e10bac9ed
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-13 09:55:04 -0300

    EX10 - feat: Os factory methods foram movidos para aqui. Estão no mesmo namespace.

---
+++ EX10/MensagemFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Miscelania;
+
+namespace Miscelania
+{
+    public static class MensagemFactory
+    {
+        public static Mensagem Alerta(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Alerta, esperarTecla, limparConsole);
+
+        public static Mensagem Erro(string conteudo, bool esperarTecla = true, bool limparConsole = true)
+            => new Mensagem(conteudo, TipoMensagem.Erro, esperarTecla, limparConsole);
+
+        public static Mensagem Informacao(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Informacao, esperarTecla, limparConsole);
+
+        public static Mensagem Sucesso(string conteudo, bool esperarTecla = true, bool limparConsole = false)
+            => new Mensagem(conteudo, TipoMensagem.Sucesso, esperarTecla, limparConsole);
+    }
+}
