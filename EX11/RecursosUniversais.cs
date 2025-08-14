314b9382685cf95366418406e587b53732af5fc2
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 16:45:36 -0300

    EX11 - feat: Criação da classe que pede dados ao usuário e uma classe que formata decimais.

---
+++ EX11/RecursosUniversais.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace UniversalResources
+{
+    public static class RequisicoesDeDados
+    {
+        public static string? EntrarComInput(string msg)
+        {
+            Console.Write(msg);
+            string? input = Console.ReadLine();
+            return input;
+        }
+    }
+    public static class FormatadorDecimal
+    {
+        public static string Formatar(double valor)
+        {
+            return valor.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
