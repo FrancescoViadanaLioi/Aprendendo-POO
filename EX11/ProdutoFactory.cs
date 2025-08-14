7461c7fc2ecc27748f4ad305a893a033eb4e6a7b
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 15:48:30 -0300

    EX11 - feat: Script Factory que cria o novo produto passando por verificações.

---
+++ EX11/ProdutoFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Product;
+using Validations;
+
+namespace Product
+{
+    public class ProdutoFactory
+    {
+        public Produto CriarProduto(string nome, double preco)
+        {
+            if (!ValidacoesDeDados.ValidarNome(nome)) throw new ArgumentNullException("O nome é inválido.");
+            if (!ValidacoesDeDados.ValidarPreco(preco, out double precoValidado )) throw new ArgumentNullException("O preco é inválido.");
+
+            return new Produto(nome, precoValidado);
+        }
+    }
+}
