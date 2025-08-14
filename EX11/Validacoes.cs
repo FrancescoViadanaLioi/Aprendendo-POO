270d458ffd49954a0a689e28b8690b7b3f9b1d17
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 16:40:59 -0300

    EX11 - feat: Sobrecarga do método de validação de preço para que ela suporte entrada de usuário via console.

--- EX11/Validacoes.cs
+++ EX11/Validacoes.cs
@@ -11,5 +11,20 @@ namespace Validations
             string padrao = @"^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[ -][A-Za-zÀ-ÖØ-öø-ÿ]+)*$";
             return !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, padrao) && input.Length > 1;
         }
+        public static bool ValidarPreco(string input, out double precoValidado)
+        {
+            return double.TryParse(input, CultureInfo.InvariantCulture, out precoValidado) && precoValidado > 0;
+        }
+        public static bool ValidarPreco(double input, out double precoValidado)
+        {
+            precoValidado = 0.00;
+
+            if (input > 0)
+            {
+                precoValidado = input;
+                return true;
+            }
+            return false;
+        }
     }
 }
