80a02c5797c9328040b55a9b53d8307ff0b2ef75
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 09:43:00 -0300

    EX10 - fix: Alteração da validação dos elementos, agora, serão alturas de pessoas para fazer sentido ao exercício didático.

---
+++ EX10/Validacoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Validacoes
+{
+    public static class ValidacoesDeDados
+    {
+        public static bool ValidarTamanhoVetor(string input, out int tamanho)
+        {
+            return int.TryParse(input, out tamanho) && tamanho > 0;
+        }
+        public static bool ValidarAltura(string input)
+        {
+            return double.TryParse(input, out double altura) && (altura > 1.20 && altura < 2.40);
+        }
+    }
+}
