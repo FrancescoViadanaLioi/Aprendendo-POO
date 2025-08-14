0df063cd8639c3f1cc55e9f55c94db5bde2545b6
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 09:50:10 -0300

    EX10 - feat: Script que realiza operações matemáticas no array.

---
+++ EX10/VetorOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vetor.Operations
+{
+    public static class OperacoesVetor
+    {
+        public static double CalcularMedia(double[] vector , int n)
+        {
+            vector = new double [n];
+            double somaDosElementos = 0;
+
+            foreach (double elemento in vector)
+            {
+                somaDosElementos += elemento;
+            }
+            return somaDosElementos / n;
+        }
+    }
+}
