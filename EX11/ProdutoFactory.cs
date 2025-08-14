edcff33a642e510798285ad3a59e339b4db45ae0
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 15:50:13 -0300

    EX11 - fix: Tranformação da classe e método para estáticos.

--- EX11/ProdutoFactory.cs
+++ EX11/ProdutoFactory.cs
@@ -5,9 +5,9 @@ using Validations;
 
 namespace Product
 {
-    public class ProdutoFactory
+    public static class ProdutoFactory
     {
-        public Produto CriarProduto(string nome, double preco)
+        public static Produto CriarProduto(string nome, double preco)
         {
             if (!ValidacoesDeDados.ValidarNome(nome)) throw new ArgumentNullException("O nome é inválido.");
             if (!ValidacoesDeDados.ValidarPreco(preco, out double precoValidado )) throw new ArgumentNullException("O preco é inválido.");
