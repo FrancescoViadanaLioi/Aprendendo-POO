5d527a1c464488ede74b3f4366d51c41c6ac86bd
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 16:35:28 -0300

    EX11 - feat: Script de mensagens que contém os principais atributos de uma mensagem, e também possui um construtor para padronizar a instânciação da mensagem.

---
+++ EX11/Mensagem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public enum TipoMensagem
+    {
+        Erro,
+        Informacao
+    };
+    public class Mensagem
+    {
+        public string Conteudo { get; }
+        public TipoMensagem Tipo { get; }
+        public bool EsperarTecla { get; }
+        public bool LimparConsole { get; }
+
+        public Mensagem(string conteudo, TipoMensagem tipo, bool esperarTecla, bool limparConsole)
+        {
+            Conteudo = conteudo;
+            Tipo = tipo;
+            EsperarTecla = esperarTecla;
+            LimparConsole = limparConsole;
+        }
+    }
+}
