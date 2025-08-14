9a6487ade8573176c6e6ce39187cd26dda64143a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-14 14:58:55 -0300

    EX11 - feat: Método que executa a validação do nome que o usuário coloca ao produto.

---
+++ EX11/Validacoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Validations
+{
+    public static class ValidacoesDeDados
+    {
+        public static bool ValidarNome(string input)
+        {
+            string padrao = @"^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[ -][A-Za-zÀ-ÖØ-öø-ÿ]+)*$";
+            return !string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, padrao) && input.Length > 1;
+        }
+    }
+}
