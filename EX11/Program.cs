fb60ac6ee31b4153c9fbb99bbc28690b004cfa2a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-20 11:27:51 -0300

    EX11 - feat: Adição de alguns itens ao fluxo principal.

---
+++ EX11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Product;
+using Product.Input;
+using Miscelania;
+using UniversalResources;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = EntradaDoUsuario.EntrarComQuantidadeDeProdutos();
+        Produto[] produtos = new Produto[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string? nome = EntradaDoUsuario.EntrarComNome(n + 1);
+            double preco = EntradaDoUsuario.EntrarComPreco(n + 1);
+            produtos[n] = new Produto { Nome = nome, Preco = preco };
+        }
+    }
+}
