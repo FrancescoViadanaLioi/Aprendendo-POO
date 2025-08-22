ce880fc2ca8195c7383b68a3e5404d38cfc555df
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-22 10:44:17 -0300

    EX12 - feat: Integração ao main.

--- EX12/EX12/Program.cs
+++ EX12/EX12/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Operations;
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        int soma = Operacoes.Sum(1, 2, 3, 4, 5, 6, 7, 8);
+        Console.WriteLine(soma);
     }
 }
