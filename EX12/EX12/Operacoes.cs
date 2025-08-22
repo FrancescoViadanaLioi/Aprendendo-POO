7669e43af96e2ff57a1ec992b12090ce162600d4
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-22 10:38:03 -0300

    EX12 - feat: Script de Operações.

---
+++ EX12/EX12/Operacoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operations
+{
+    internal class Operacoes
+    {
+        public static int Sum(params int[] nums)
+        {
+            int soma = 0;
+            foreach (int valor in nums)
+            {
+                soma += valor;
+            }
+            return soma;
+        }
+    }
+}
