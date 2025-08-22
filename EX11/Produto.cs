515377522dedb75fb1c3dd0ec097eecbe8884e01
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-22 10:03:43 -0300

    EX11 - feat: Adição do método que retorna a média dos preços.

---
+++ EX11/Produto.cs
@@ -0,0 +1,27 @@
+﻿using Product.Input;
+using System;
+using System.Transactions;
+using Validations;
+
+namespace Product
+{
+
+    public class Produto
+    {
+        public string? Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto() { }
+        public Produto(string nome, double preco)
+        {
+            if (!ValidacoesDeDados.ValidarNome(nome)) throw new ArgumentException("O nome é inválido");
+            if (!ValidacoesDeDados.ValidarPreco(preco, out double precoValidado)) throw new ArgumentException("O preço é inválido.");
+            Nome = nome;
+            Preco = precoValidado;
+        }
+        public static double CalcularMedia(double soma, int n)
+        {
+            return soma / n;
+        }
+    }
+}
