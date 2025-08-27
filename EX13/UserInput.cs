55759ef7fa02f4eb0cac7c4af3ba22928c0a6e09
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-27 09:33:17 -0300

    EX13 - feat: Classe com métodos que fazem a requisição de dados do usuário.

---
+++ EX13/UserInput.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Validations;
+using Miscelania;
+using UniversalResources;
+
+namespace Employee.Input
+{
+    public static class EntradaDoUsuario
+    {
+        public static string EntrarComNome(int n)
+        {
+            while (true)
+            {
+                string? input = RequisicaoDeDados.PedirEntrada($"Digite o nome do funcionário {n}: ");
+                if (input != null && Validacoes.ValidarNome(input)) return input;
+                ConsoleUI.ExibirErro("Verifique se o nome tem somente letras, e se necessário, hífens ou apóstrofo.");
+            }
+        }
+        public static string EntrarComID(int n)
+        {
+            while (true)
+            {
+                string? input = RequisicaoDeDados.PedirEntrada($"Digite o ID do funcionário {n}: ");
+                if (input != null && Validacoes.ValidarID(input)) return input;
+                ConsoleUI.ExibirErro("Verifique se o ID tem é composto por somente 3 números inteiros positivos.");
+            }
+        }
+        public static double EntrarComSalario(int n)
+        {
+            while (true)
+            {
+                string? input = RequisicaoDeDados.PedirEntrada($"Digite o salário do funcionário {n}: R$");
+                if (input != null && Validacoes.ValidarSalario(input, out double salarioValidado)) return salarioValidado;
+                ConsoleUI.ExibirErro("Verifique se o salário é pelo menos um salário mínimo (R$1500) e se é um número válido.");
+            }
+        }
+        public static int EntrarComNumeroValido()
+        {
+            while (true)
+            {
+                string? input = RequisicaoDeDados.PedirEntrada("Quantos funcionários serão analisados? ");
+                if (input != null && Validacoes.ValidarNumeroDeFuncionarios(input, out int numeroValidado)) return numeroValidado;
+                ConsoleUI.ExibirErro("Verifique se você digitou um número positivo inteiro.");
+            }
+        }
+    }
+}
