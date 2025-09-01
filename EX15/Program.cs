1db565a3304a214f823fe1a59d17234172de8573
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-01 11:57:24 -0300

    EX15 - feat: Integração ao Main.

--- EX15/Program.cs
+++ EX15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EX15.Entities;
+using EX15.Entities.Enums;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        DateTime d1 = new DateTime(2025, 08, 29, 15, 46, 32, DateTimeKind.Local);
+
+        Order order = new Order("ABC34", d1, OrderStatus.PendingPayment);
+
+        Console.WriteLine($"{order.Id}, {order.Moment}, {order.Status}");
+
+    }
+}
