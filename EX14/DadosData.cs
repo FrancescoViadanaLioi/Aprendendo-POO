07216f06ce55e81700cff7f31754784421e0e121
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-01 11:14:10 -0300

    EX14 - feat: Exercício feito para testar e usar operações e propriedades da classe DateTime.

---
+++ EX14/DadosData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Saida
+{
+    public static class SaidaDeDados
+    {
+        public static void ImprimirDadosData(DateTime date)
+        {
+            Console.WriteLine($"Data original: {date}");
+            Console.WriteLine($"Tipo: {date.Kind}");
+            Console.WriteLine($"Data para local: {date.ToLocalTime()}");
+            Console.WriteLine($"Data para UTC: {date.ToUniversalTime()}");
+            Console.WriteLine($"Data para formato ISO 8601: {date.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")}");
+        }
+    }
+}
--- EX14/Program.cs
+++ EX14/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Globalization;
+using System.ComponentModel;
+using Saida;
 
 class Program
 {
     static void Main(string[] args)
     {
-        TimeSpan t1 = TimeSpan.FromDays(1.25);
-        Console.WriteLine(t1);
+        DateTime d1 = DateTime.Parse("2007-02-01T22:00:00Z");
+        DateTime d2 = DateTime.Parse("2007-08-09 22:00:00");
+
+        SaidaDeDados.ImprimirDadosData(d1);
+        Console.WriteLine();
+        SaidaDeDados.ImprimirDadosData(d2);
     }
+
+    private static string Formatar(double num)
+    {
+        return num.ToString("F2", CultureInfo.InvariantCulture);
+    }
+
 }
