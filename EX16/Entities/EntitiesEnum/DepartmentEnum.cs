1d3fa5c9605f9e93b19f2639b5f89e9dfe6809f5
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-03 09:38:58 -0300

    EX15 - feat: Criação do enum dos níveis de senioridade.

---
+++ EX16/Entities/EntitiesEnum/DepartmentEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EX16.Entities.EntitiesEnum
+{
+    enum AvailabeDepartments : int
+    { 
+        IT = 0,
+        HR = 1,
+        Administration = 2,
+        Design = 3,
+        Marketing = 4,
+        Accountability = 5,
+    }
+}
---
+++ EX16/Entities/EntitiesEnum/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EX16.Entities.EntitiesEnum
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
