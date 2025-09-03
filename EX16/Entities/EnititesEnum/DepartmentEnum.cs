d0bc81f944f9a875976cd50fe2fb5c72c3010947
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-03 09:27:32 -0300

    EX16 - fix: Ajuste da enumeração para que evite bugs.

--- EX16/Entities/EnititesEnum/DepartmentEnum.cs
+++ EX16/Entities/EnititesEnum/DepartmentEnum.cs
@@ -2,14 +2,13 @@
 
 namespace EX16.Entities.EntitiesEnum
 {
-    enum AvailabeDepartments
+    enum AvailabeDepartments : int
     { 
-        IT,
-        HR,
-        Administration,
-        Design,
-        Marketing,
-        Accountability,
-
+        IT = 0,
+        HR = 1,
+        Administration = 2,
+        Design = 3,
+        Marketing = 4,
+        Accountability = 5,
     }
 }
