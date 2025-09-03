5fd5419bcd93a57000c22eca343e1393a95597e4
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-03 08:57:39 -0300

    EX16 - feat: Criação da entidade Department que possui o atributo Name que contém o nome do departamento que o funcionário trabalha. Além disso, também foram adicionados construtores dessa entidade.

---
+++ EX16/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EX16.Entities.EntitiesEnum;
+
+namespace EX16.Entities
+{
+    internal class Department
+    {
+        public AvailabeDepartments Name{ get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(AvailabeDepartments name)
+        {
+            Name = name;
+        }
+    }
+}
