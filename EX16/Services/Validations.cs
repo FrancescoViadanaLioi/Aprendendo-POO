cfd2b7dfe2b99efb45d0bec6ea0f0a42a35e61ea
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-04 16:50:47 -0300

    EX16 - feat: Ajuste das validações e adição de um método de validar a quantidade de contratos. Além disso, foi feita a correção do nome do método "EnterWithSalary" para "EnterWithBaseSalary".

--- EX16/Services/Validations.cs
+++ EX16/Services/Validations.cs
@@ -11,23 +11,27 @@ namespace EX16.Services
     {
         public static bool ValidateName(string input)
         {
-            return input != null && Regex.IsMatch(input, @"^[\p{L}]+(?:[ '-][\p{L}]+)*$");
+            return string.IsNullOrWhiteSpace(input) && Regex.IsMatch(input, @"^[\p{L}]+(?:[ '-][\p{L}]+)*$");
         }
         public static bool ValidateLevel(string input, out WorkerLevel level)
         {
             return Enum.TryParse(input, ignoreCase: true, out level) && Enum.IsDefined(typeof(WorkerLevel), level);
         }
-        public static bool ValidateSalary(string input, out double salary)
+        public static bool ValidateBaseSalary(string input, out double salary)
         {
             return double.TryParse(input, CultureInfo.InvariantCulture, out salary) && salary > 0;
         }
-        public static bool ValidateSalary(double salary)
+        public static bool ValidateBaseSalary(double salary)
         {
             return salary > 0;
         }
         public static bool ValidateDepartment(string input, out AvailableDepartments department)
         {
             return Enum.TryParse(input, ignoreCase: true, out department) && Enum.IsDefined(typeof(AvailableDepartments), department);
+        }
+        public static bool ValidateContracts(string input, out int contracts)
+        {
+            return int.TryParse(input, out contracts) && contracts > 0;
         }
     }
 }
