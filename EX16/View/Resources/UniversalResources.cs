ec7ffd552fde2e7c638a3acd03c905860ea8d310
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-04 09:13:42 -0300

    EX16 - feat: Adição de recursos universais ao código.

---
+++ EX16/View/Resources/UniversalResources.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace EX16.View.Resources
+{
+    internal static class UniversalResources
+    {
+        public static string? AskForInput(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+    }
+    internal static class Formatter
+    {
+        public static string Format(double value)
+        {
+            return value.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
