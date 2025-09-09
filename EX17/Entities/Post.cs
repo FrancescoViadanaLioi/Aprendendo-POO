a6f2f26f6988e6088d5d903e7a1d4adc6272aa96
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-09 10:59:44 -0300

    EX17 - feat: Entidade Post que possui os principais atributos de um post: título, texto, número de likes e quantidade de comentários armazenados numa lista.

---
+++ EX17/Entities/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using EX17.Entities;
+
+namespace EX17.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post(DateTime moment, string title, string text, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Text = text;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+    }
+}
