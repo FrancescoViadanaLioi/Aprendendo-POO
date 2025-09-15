3890cc193e8dc099a4652e1e67890c0c9ee51a8a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-15 11:40:16 -0300

    EX18 - feat: Criação das entidades Account e BusinessAccount, em que a segunda é derivada da primeira.

---
+++ EX18/Script/Entities/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX18.Entities
+{
+    internal class Account
+    {
+        public string Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account(string number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ EX18/Script/Entities/BusinessAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX18.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; private set; }
+
+        public BusinessAccount(string number, string name, double balance, double loanLimit) : base(number, name, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
