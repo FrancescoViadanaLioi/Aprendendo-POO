44e4ed622352382dc83b9e760cab104a3ddbb847
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 10:49:25 -0300

    EX18 - feat: Novo tipo de conta: Conta de Poupança.

---
+++ EX18/Script/Entities/SavingAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace EX18.Entities
+{
+    internal class SavingAccount: Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount(string number, string name, double balance, double interestRate) : base(number, name, balance)
+        {
+            InterestRate = interestRate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
