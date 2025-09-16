9cb72c5bd14a92bd74a8c0e3c7386a654c584b40
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 17:56:27 -0300

    EX19 - feat: Primeira classe "Produto", que será a classe mãe de determinadas entidades.

---
+++ EX19/SourceCode/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EX19.Entities
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string PriceTag()
+        {
+            return $"{Name}: ${Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
