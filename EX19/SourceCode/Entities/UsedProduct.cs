35d1e9fe5bfe8e38fe768e73f4a8c00bc3ab574f
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-16 18:23:24 -0300

    EX19 - feat: Classe herdeira de Product que possui agora a data de fabricação do produto.

---
+++ EX19/SourceCode/Entities/UsedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EX19.Entities
+{
+    internal sealed class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; private set; }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string PriceTag()
+        {
+            return $"{Name} (used): ${Price.ToString("F2", CultureInfo.InvariantCulture)} (Manufacture Date: {ManufactureDate.ToString("dd/MM/yyyy")}";
+        }
+    }
+}
