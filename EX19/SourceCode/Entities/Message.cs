da63390f746a1495dd558775816f44f873bf7dea
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-17 09:30:13 -0300

    EX19 - feat: Entidade de mensagem que possui os principais atributos e o construtor da mensagem.

---
+++ EX19/SourceCode/Entities/Message.cs
@@ -0,0 +1,23 @@
+﻿using EX19.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX19.Entities
+{
+    internal class Message
+    {
+        public string Text { get; private set; }
+        public MessageKind Kind { get; private set; }
+        public bool Pause { get; private set; }
+
+        public Message(string text, MessageKind kind, bool pause)
+        {
+            Text = text;
+            Kind = kind;
+            Pause = pause;
+        }
+    }
+}
