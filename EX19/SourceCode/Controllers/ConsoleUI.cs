ab7525aca4c1f838d48503928ff32ebcdb3107ac
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-17 09:42:01 -0300

    EX19 - feat: Classe de ConsoleUI que agora exibe as mensagens para cada tipo definidamente.

---
+++ EX19/SourceCode/Controllers/ConsoleUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EX19.Entities;
+using EX19.Entities.Enum;
+
+namespace EX19.View
+{
+    internal class ConsoleUI
+    {
+        public static void ShowError(string text)
+        {
+            ShowMessage(new Message(text, MessageKind.Error, pause: true));
+        }
+        public static void ShowInfo(string text)
+        {
+            ShowMessage(new Message(text, MessageKind.Info, pause: true));
+        }
+        private static void ShowMessage(Message message)
+        {
+            string prefix = message.Kind.ToString().ToUpper();
+            Console.WriteLine($"{prefix}: {message.Text}");
+
+            if (message.Pause)
+            {
+                Console.WriteLine("(Press any key to continue)");
+            }
+        }
+    }
+}
