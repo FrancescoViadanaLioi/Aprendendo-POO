8a590e338bb712323a748191031fb0a0ca803719
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-17 09:27:07 -0300

    EX19 - feat: Recursos universais que contém classes e métodos que são podem ser usados por todos os scripts do programa.

---
+++ EX19/SourceCode/Controllers/UniversalResources.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EX19.Controllers;
+
+namespace EX19.Controllers
+{
+    internal static class DataInput
+    {
+        public static string? AskForInput(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+    }
+    internal static class Formatter
+    {
+        public static string Format(double value)
+        {
+            return value.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+    internal static class InputHelper
+    {
+        public static T GetValidatedInput<T>(
+            string prompt,
+            Func<string, bool> validator,
+            Func<string, T> converter,
+            string errorMessage)
+        {
+            while (true)
+            {
+                string? input = DataInput.AskForInput(prompt);
+                if (input != null && validator(input)) return converter(input);
+                ConsoleUI
+            }
+        }
+    }
+}
