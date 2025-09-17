a36de5519040b91c51409f70d2579c50e775b674
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-17 09:26:10 -0300

    EX19 - feat: Enum que contém os tipos da mensagem.

---
+++ EX19/SourceCode/Entities/Enum/MessageKind.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX19.Entities.Enum
+{
+    internal enum MessageKind
+    {
+        Error = 0,
+        Info = 1
+    }
+}
