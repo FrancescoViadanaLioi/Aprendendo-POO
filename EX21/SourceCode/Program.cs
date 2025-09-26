b0c6fed87759e31699410c98d3c14a662e00f942
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-26 16:18:39 -0300

    EX21 - feat: Primeiro commit.

---
+++ EX21/SourceCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\ProjetosTi\C#\POO\EX21";
+
+        Console.WriteLine($"Initializing copy...");
+        try
+        {
+            IEnumerable<string> folders = Directory.EnumerateDirectories(path, ".", SearchOption.AllDirectories);
+            Console.WriteLine("FOLDERS:");
+            foreach (string s in folders)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Criando pasta...");
+            Directory.CreateDirectory(@"C:\ProjetosTi\C#\POO\EX21\Teste01");
+            Console.WriteLine("Pasta criada com sucesso.");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"This folder doesn't exist: {e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine(Path.GetTempPath());
+        }
+    }
+}
