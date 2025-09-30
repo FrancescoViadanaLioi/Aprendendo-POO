d0033e4c434d05e45d5993690b50ad130378c768
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-30 17:22:03 -0300

    EX21 - Feat: Implementação ao main.

--- EX21/SourceCode/Program.cs
+++ EX21/SourceCode/Program.cs
@@ -1,34 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
+﻿using EX21.Entities;
+using EX21.Entities.Interfaces;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string path = @"C:\ProjetosTi\C#\POO\EX21";
+        Kriptonian superman = new Kriptonian("Superman");
 
-        Console.WriteLine($"Initializing copy...");
-        try
-        {
-            IEnumerable<string> folders = Directory.EnumerateDirectories(path, ".", SearchOption.AllDirectories);
-            Console.WriteLine("FOLDERS:");
-            foreach (string s in folders)
-            {
-                Console.WriteLine(s);
-            }
-
-            Console.WriteLine("Criando pasta...");
-            Directory.CreateDirectory(@"C:\ProjetosTi\C#\POO\EX21\Teste01");
-            Console.WriteLine("Pasta criada com sucesso.");
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine($"This folder doesn't exist: {e.Message}");
-        }
-        finally
-        {
-            Console.WriteLine(Path.GetTempPath());
-        }
+        superman.SuperSpeed();
     }
 }
