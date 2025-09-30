3496199a1be993c4662c818d827ab8953fbaef5d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-30 17:21:48 -0300

    EX21 - feat: Classe Kriptoniano que herda a interface IPowers.

---
+++ EX21/SourceCode/Entities/Kriptonian.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EX21.Entities.Interfaces;
+
+namespace EX21.Entities
+{
+    internal class Kriptonian: IPowers
+    {
+        public string Name { get; private set; }
+
+        public Kriptonian(string name)
+        {
+            Name = name;
+        }
+
+        public void HeatVision()
+        {
+            Activate("Heat Vision");
+        }
+        public void FreezingBreath()
+        {
+            Activate("Freezing Breath");
+        }
+        public void Flight()
+        {
+            Console.WriteLine($"Is it a bird? Is it a plane? No... it's {this.Name}!");
+        }
+        public void SuperStrength()
+        {
+            Activate("Super strength");
+        }
+        public void XRayVision()
+        {
+            Activate("X-Ray Vision");
+        }
+        public void SuperSpeed()
+        {
+            Console.WriteLine($"Faster than a speeding bullet: it's {this.Name}!");
+        }
+        private void Activate(string power)
+        {
+            Console.WriteLine($"{this.Name} is using {power}!");
+        }
+    }
+}
