7216c47c66dda99095a8dc1c20bcee3a18b37f5a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-30 17:31:55 -0300

    EX21 - fix: Simplificação dos métodos.

--- EX21/SourceCode/Entities/Kriptonian.cs
+++ EX21/SourceCode/Entities/Kriptonian.cs
@@ -3,47 +3,27 @@ using EX21.Entities.Interfaces;
 
 namespace EX21.Entities
 {
-    internal class Kriptonian: IPowers
+    internal class Kriptonian : IPowers
     {
         public string Name { get; private set; }
-        public string? SecretIdentity { get; private set; } = null;
+        public string? SecretIdentity { get; private set; }
 
         public Kriptonian(string name)
         {
             Name = name;
         }
-        public Kriptonian(string name, string secretIdentity): this(name)
+        public Kriptonian(string name, string secretIdentity) : this(name)
         {
             SecretIdentity = secretIdentity;
         }
 
-        public void HeatVision()
-        {
-            Activate("Heat Vision");
-        }
-        public void FreezingBreath()
-        {
-            Activate("Freezing Breath");
-        }
-        public void Flight()
-        {
-            Console.WriteLine($"Is it a bird? Is it a plane? No... it's {this.Name}!");
-        }
-        public void SuperStrength()
-        {
-            Activate("Super strength");
-        }
-        public void XRayVision()
-        {
-            Activate("X-Ray Vision");
-        }
-        public void SuperSpeed()
-        {
-            Console.WriteLine($"Faster than a speeding bullet: it's {this.Name}!");
-        }
-        private void Activate(string power)
-        {
-            Console.WriteLine($"{this.Name} is using {power}!");
-        }
+        public void HeatVision() => Activate("Heat Vision");
+        public void FreezingBreath() => Activate("Freezing Breath");
+        public void Flight() => Console.WriteLine($"Is it a bird? Is it a plane? No... it's {Name}!");
+        public void SuperStrength() => Activate("Super strength");
+        public void XRayVision() => Activate("X-Ray Vision");
+        public void SuperSpeed() => Console.WriteLine($"Faster than a speeding bullet: it's {Name}!");
+
+        private void Activate(string power) => Console.WriteLine($"{Name} is using {power}!");
     }
 }
