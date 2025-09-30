a484b124a20da1c151b3dfe35d419182b1f63dc2
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-09-30 17:21:13 -0300

    EX21 - feat: Interface que estabelece os poderes básicos de um kriptoniano.

---
+++ EX21/SourceCode/Entities/Interfaces/IPowers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX21.Entities.Interfaces
+{
+    internal interface IPowers
+    {
+        void HeatVision();
+        void FreezingBreath();
+        void Flight();
+        void SuperStrength();
+        void XRayVision();
+        void SuperSpeed();
+    }
+}
