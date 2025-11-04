e87ba604e0582aa7012042a983f0c223118d3fcf
FrancescoViadanaLioi <francesco.viadanalioi@gmail.com>
2025-11-04 18:32:54 -0300

    First commit.

---
+++ EX26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EX26.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+
+        Console.Write("Type how many numbers your list will contain: ");
+
+        int n = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Type the number of the {i + 1}° position: ");
+            int x = int.Parse(Console.ReadLine()!);
+            list.Add(x);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"The greatest element: {CalculationService.Max(list)}");
+    }
+}
