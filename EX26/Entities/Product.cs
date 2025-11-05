d65411541f4ababb7649ec807cfa81046358dd99
FrancescoViadanaLioi <francesco.viadanalioi@gmail.com>
2025-11-04 23:47:14 -0300

    EX26: Exercise which gets the product with the highest price and return its name and price.

---
+++ EX26/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX26.Entities
+{
+    internal class Product : IComparable
+    {
+        public string Name { get; private set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException($"Comparing error: Argument is not a product.");
+            }
+            if (obj == null)
+            {
+                throw new ArgumentNullException("Null error: The object cannot be null.");
+            }
+            Product p = obj as Product;
+            return Price.CompareTo(p.Price);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}; Price: ${this.Price.ToString(CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- EX26/Program.cs
+++ EX26/Program.cs
@@ -1,24 +1,34 @@
 ﻿using System;
+using System.Globalization;
 using EX26.Services;
+using EX26.Entities;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> list = new List<int>();
+        List<Product> products = new List<Product>();
 
-        Console.Write("Type how many numbers your list will contain: ");
-
+        Console.Write("Type how many products will be analyzed: ");
         int n = int.Parse(Console.ReadLine()!);
 
+        Console.WriteLine("Enter with the products name and price, following this model: 'name','price'");
         for (int i = 0; i < n; i++)
         {
-            Console.Write($"Type the number of the {i + 1}° position: ");
-            int x = int.Parse(Console.ReadLine()!);
-            list.Add(x);
+            Console.Write($"{i + 1}° Product: ");
+            string[] vect = Console.ReadLine()!.Split(',');
+            string name = vect[0];
+            double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+            Product product = new Product(name, price);
+            products.Add(product);
         }
+
         Console.WriteLine();
 
-        Console.WriteLine($"The greatest element: {CalculationService.Max(list)}");
+        Product highestPrice = CalculationService.Max(products);
+
+        Console.WriteLine("Product with the highest price:");
+        Console.WriteLine(highestPrice.ToString());
     }
 }
